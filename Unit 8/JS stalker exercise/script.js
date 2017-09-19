afcf31540bb2e29a0454cc861f20f0d6942748e4
AfkZa4aem <nastypinagrably@gmail.com>
2017-09-19 16:26:56 +0300

    Js exercise done

---
+++ Unit 8/JS stalker exercise/script.js
@@ -0,0 +1,8 @@
+var userName = prompt("Hello! What is your name?");
+var userSecondName = prompt("What is your second name?");
+var userAge = prompt("How old are you?");
+
+var fullName = userName + " " + userSecondName
+
+console.log("Nice to meet you, " + fullName);
+console.log("You are " + userAge + " years old!");
