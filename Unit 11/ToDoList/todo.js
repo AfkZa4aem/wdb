7389d1e4848f471b56ebc5f787f06d17ce7a7014
AfkZa4aem <nastypinagrably@gmail.com>
2017-10-10 11:03:12 +0300

    refactoring todo app
    add fumctions cleaned loop

--- Unit 11/ToDoList/todo.js
+++ Unit 11/ToDoList/todo.js
@@ -4,29 +4,39 @@ var answer = prompt("what would you like to do?");
 while(answer !== "quit") {
 	// handle input
 	if(answer === "list") {
-		console.log("*************");
-		toDo.forEach(function(todo, i){
-			console.log(i + ": " + todo);
-		});
-		console.log("*************");
+		listTodoes();
 	}
 	else if (answer === "new") {
-		// ask for new todo
-		var newTodo = prompt("Enter new todo");
-		// add todo to array
-		toDo.push(newTodo);
-		console.log("Todo was successfully added")
+		newTodo();
 	}
 	else if (answer === "delete") {
-		// ask for index of todo to be deleted
-		var index = prompt("Enter index of todo:");
-		// delete that todo
-		// using splice() method
-		// will delete toDo[index]
-		toDo.splice(index, 1);
-		console.log("Todo was deleted");
+		deleteTodo();
 	}
 
 	var answer = prompt("what would you like to do?");
 }
 console.log("Ok, you quit the app");
+
+function listTodoes(){
+	console.log("*************");
+	toDo.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("*************");
+}
+function newTodo(){
+	// ask for new todo
+	var newTodo = prompt("Enter new todo");
+	// add todo to array
+	toDo.push(newTodo);
+	console.log("Todo was successfully added");
+}
+function deleteTodo(){
+	// ask for index of todo to be deleted
+	var index = prompt("Enter index of todo:");
+	// delete that todo
+	// using splice() method
+	// will delete toDo[index]
+	toDo.splice(index, 1);
+	console.log("Todo was deleted");
+}
