4e15b9afd7f3258c43159c1dcd4eb18e4d3eb2b5
AfkZa4aem <nastypinagrably@gmail.com>
2017-10-10 10:45:35 +0300

    improve "todo" app
    add delete function

--- Unit 11/ToDoList/todo.js
+++ Unit 11/ToDoList/todo.js
@@ -1,12 +1,30 @@
 var toDo = ["achoo"];
+
 var answer = prompt("what would you like to do?");
 while(answer !== "quit") {
+	// handle input
 	if(answer === "list") {
-		console.log(toDo);
+		console.log("*************");
+		toDo.forEach(function(todo, i){
+			console.log(i + ": " + todo);
+		});
+		console.log("*************");
 	}
 	else if (answer === "new") {
+		// ask for new todo
 		var newTodo = prompt("Enter new todo");
+		// add todo to array
 		toDo.push(newTodo);
+		console.log("Todo was successfully added")
+	}
+	else if (answer === "delete") {
+		// ask for index of todo to be deleted
+		var index = prompt("Enter index of todo:");
+		// delete that todo
+		// using splice() method
+		// will delete toDo[index]
+		toDo.splice(index, 1);
+		console.log("Todo was deleted");
 	}
 
 	var answer = prompt("what would you like to do?");
