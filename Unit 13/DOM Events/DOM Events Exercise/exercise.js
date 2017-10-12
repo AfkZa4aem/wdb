9fa82da78a4185888532a83c47ceb47f0b8cfdf2
Unknown <nastypinagrably@gmail.com>
2017-10-12 20:53:00 +0300

    exercise Colt solution

--- Unit 13/DOM Events/DOM Events Exercise/exercise.js
+++ Unit 13/DOM Events/DOM Events Exercise/exercise.js
@@ -1,11 +1,16 @@
 var button = document.querySelector("button");
-var body = document.querySelector("body");
+
+// var body = document.querySelector("body");
+
+// button.addEventListener("click", function(){
+// 	if(body.style.background !== "purple"){
+// 		body.style.background = "purple";
+// 	}
+// 	else{
+// 		body.style.background = "white";
+// 	}
+// })
 
 button.addEventListener("click", function(){
-	if(body.style.background !== "purple"){
-		body.style.background = "purple";
-	}
-	else{
-		body.style.background = "white";
-	}
+	document.body.classList.toggle("purple");
 })
