af8db72e45b2110aaf985d133bc1afde49d92191
Unknown <nastypinagrably@gmail.com>
2017-10-14 07:00:16 +0300

    add comments

--- Unit 15/Color picker/colorGame.js
+++ Unit 15/Color picker/colorGame.js
@@ -1,3 +1,4 @@
+// create array of colors
 var colors = [
 	"rgb(255, 0, 0)",
 	"rgb(255, 255, 0)",
@@ -7,12 +8,17 @@ var colors = [
 	"rgb(255, 0, 255)"
 ];
 
+// pick all divs to array
 var squares = document.querySelectorAll(".square");
+// use one of the arrays as "picked"
 var pickedColor = colors[3];
+// add variable for span
 var colorDisplay = document.getElementById("colorDisplay");
 
+// change <span> text content 
 colorDisplay.textContent = pickedColor;
 
+// iterate colors array
 for(var i = 0; i < squares.length; i++){
 	// add initial colors ro squares
 	squares[i].style.backgroundColor = colors[i];
