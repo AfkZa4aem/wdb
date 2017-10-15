30011a1ed91cfabc1def5a66d41e77ffa8806f6e
Unknown <nastypinagrably@gmail.com>
2017-10-15 16:24:27 +0300

    update
    add Colt solution

--- Unit 16/JQExercise/exercise.js
+++ Unit 16/JQExercise/exercise.js
@@ -1,4 +1,8 @@
 $("div").css("backgroundColor", "purple");
 $(".highlight").css("width", "200px");
 $("#third").css("border", "2px solid orange");
-$(".highlight:first").css("color", "pink");
+// $(".highlight:first").css("color", "pink");
+
+// Colt solution faster then :first, because of
+// first-of-type built-in in css
+$("div:first-of-type").css("color", "pink");
