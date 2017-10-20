b4db7df1f20d40c3ba9fe0df220d73ff5b62de40
Unknown <nastypinagrably@gmail.com>
2017-10-20 09:11:09 +0300

    Making API Requests with Node

---
+++ Unit_25/APIs/first_request.js
@@ -0,0 +1,19 @@
+var request = require("request");
+
+request('http://www.rasdafafgwadf.com', function(error, response, body){
+	if(error){
+		console.log("Something went wrong!");
+		console.log(error);
+	} else {
+		if(response.statusCode == 200){
+			// Things worked!
+			console.log(body);
+		}
+	}
+});
+
+// request('http://www.google.com', function(error, response, body){
+// 	if(!error && response.statusCode == 200){
+// 		console.log(body);
+// 	}
+// });
