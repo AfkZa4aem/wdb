90dbea567087d3795100570b336aa99ccb91f049
Maxim <nastypinagrably@gmail.com>
2017-10-26 08:04:30 +0000

    YelpCamp: Creating Comments Pt.2

--- YelpCamp/v4/app.js
+++ YelpCamp/v4/app.js
@@ -3,6 +3,7 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
 	Campground = require("./models/campground"),
+	Comment = require("./models/comment"),
 	seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp_v4", {userMongoClient: true});
@@ -74,6 +75,29 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 			console.log(err);
 		} else {
 			res.render("comments/new", {campground: campground});
+		}
+	});
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	// lookup campground usig ID
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			// create new comment
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			})
+			// connect new comment to campgrouns
+			// redirect campground show page
 		}
 	});
 });
